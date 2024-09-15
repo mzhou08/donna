13071df7a4aad9770c0aec1cb5761e5e45a0d379
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-09-14 23:56:17 -0400

    regenerated code

--- convex/http.ts
+++ convex/http.ts
@@ -1,6 +1,7 @@
 import { httpRouter } from "convex/server";
 import { message } from "./functions";
 import { userByAgentAddress, userByName } from "./queries";
+import { getUserResponseStatus } from "./response";
 
 const http = httpRouter();
 
@@ -20,6 +21,12 @@ http.route({
   path: "/user/agent-address",
   method: "GET",
   handler: userByAgentAddress,
+})
+
+http.route({
+  path: "/user/response-status",
+  method: "GET",
+  handler: getUserResponseStatus,
 })
 
 export default http;
