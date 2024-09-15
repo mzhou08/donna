fd790679e8ee748b8204817fb138bd6f97ec15ef
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-09-14 22:00:54 -0400

    userByName and userByAgentAddress helpers

--- convex/queries.ts
+++ convex/queries.ts
@@ -3,7 +3,60 @@ import { query } from "./_generated/server";
 import { api } from "./_generated/api";
 import { httpAction } from "./_generated/server";
 
-export const getUser = query({
+export const getUserByAgentAddress = query({
+  args: {
+    agentAddress: v.string(),
+  },
+
+  handler: async (ctx, args) => {
+    const userData = await ctx.db
+      .query("users")
+      .filter((q) =>
+        q.eq(q.field("agentAddress"), args.agentAddress))
+      .collect();
+
+    return {
+        userData: userData.length > 0 ? userData[0] : null,
+    };
+  },
+});
+
+export const userByAgentAddress = httpAction(async (ctx, request) => {
+  const req = await request.json();
+
+  if (req) {
+    const agentAddress = req.agentAddress;
+
+    const {userData} = await ctx.runQuery(api.queries.getUserByAgentAddress, {
+      agentAddress: agentAddress,
+    });
+
+    if (userData) {
+      return new Response(
+        new Blob([
+            JSON.stringify({
+                phone: userData.phone,
+                name: userData.name,
+            })
+        ]),
+        {
+            status: 200,
+        }
+      );
+    } else {
+      return new Response(null, {
+        status: 200,
+      });
+    }
+  }
+
+  return new Response(null, {
+    status: 400,
+  });
+});
+
+
+export const getUserByName = query({
   args: {
     name: v.string(),
   },
@@ -24,20 +77,20 @@ export const getUser = query({
   },
 });
 
-export const message = httpAction(async (ctx, request) => {
+export const userByName = httpAction(async (ctx, request) => {
   const req = await request.json();
 
   if (req) {
     const name = req.name;
 
-    const {userData} = await ctx.runQuery(api.queries.getUser, {
+    const {userData} = await ctx.runQuery(api.queries.getUserByName, {
       name: name,
     });
 
     if (userData) {
       return new Response(
         new Blob([
-            JSON.stringify({phone: userData.phone,})
+            JSON.stringify({agentAddress: userData.agentAddress,})
         ]),
         {
             status: 200,
